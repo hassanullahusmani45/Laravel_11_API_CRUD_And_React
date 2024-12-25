c40a0bdffb82e3d8b0874a4430ed841257c6e423
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2024-12-25 22:19:00 +0430

    Feat: add the relationshipe of Users and Posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use app\Models\User;
 
 class Post extends Model
 {
@@ -11,4 +12,8 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable=["title","content","user_id"];
+
+    public function user(){
+        return $this->hasOne(User::class);
+    }
 }
